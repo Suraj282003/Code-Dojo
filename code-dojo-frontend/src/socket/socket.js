38a4f839bc5fb99acc404c0db0f3cfaa9d3450bb
Suraj Yadav <92665722+Suraj282003@users.noreply.github.com>
2026-07-16 16:20:44 +0530

    Update socket connection URL environment variable

--- code-dojo-frontend/src/socket/socket.js
+++ code-dojo-frontend/src/socket/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 let socket;
 
 export const connectSocket = (token) => {
-  socket = io(process.env.VITE_API_URL, {
+  socket = io(process.env.REACT_APP_API_URL, {
     auth: { token },
     transports: ["websocket"],
   });
